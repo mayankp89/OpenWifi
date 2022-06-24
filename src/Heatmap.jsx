4fce3e402c3e6a95c1aff0f1f69b9667f809ebee
mayank <mayankshukla8931@gmail.com>
2022-06-24 11:00:21 +0530

    No_of_devices added

--- src/Heatmap.jsx
+++ src/Heatmap.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 import Progress from "./Progress";
+import {useDispatch} from 'react-redux';
+import { actionCreators } from './state/index';
+import { useSelector } from 'react-redux/es/exports';
+
 const Heatmap=()=>{
+    const dispatch = useDispatch();
+    var No_of_devices = 5;
+    const amount = useSelector(state=>state.amount)
+
     return(
         <>
            <Progress done="40"/>
-          <Link to="/upload/heatmap/select_support_plan"><button type="button" class="btn btn-outline-success" >continue</button></Link>
+          <Link to="/upload/heatmap/select_support_plan"><button type="button" class="btn btn-outline-success"  onClick={()=>{dispatch(actionCreators.depositMoney(amount*(No_of_devices-1)))}}>continue</button></Link>
         </>
     )
 }
