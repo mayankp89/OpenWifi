b059992631f3436b852060d70863b347d07bbb1a
mayank <mayankshukla8931@gmail.com>
2022-07-06 12:00:41 +0530

    Heatmap page changed

--- src/Heatmap.jsx
+++ src/Heatmap.jsx
@@ -1,44 +1,60 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 import Progress from "./Progress";
-import {ref,onValue} from "firebase/database"
-import {storage,database} from "./firebase";
-import {useDispatch} from 'react-redux';
+import { ref, onValue } from "firebase/database"
+import { storage, database } from "./firebase";
+import { useDispatch } from 'react-redux';
 import { actionCreators } from './state/index';
 import { useSelector } from 'react-redux/es/exports';
 import { useLocation } from 'react-router-dom'
 import { faAlignCenter } from '@fortawesome/free-solid-svg-icons';
-import "./heatmap.css"
+import "./Heatmap.css"
 
-const Heatmap=()=>{
+const Heatmap = () => {
     const dispatch = useDispatch();
-    var No_of_devices = 5;
-    const amount = useSelector(state=>state.amount)
+    var No_of_devices1 = 5;
+    var No_of_devices2 = 4;
+    const amount = useSelector(state => state.amount)
     const location = useLocation()
     const { Plan, price } = location.state
 
-   const [url,setUrl]=useState(); 
-useEffect(()=>{
-    const usersRef=ref(database,'/users');
-    onValue(usersRef,(snapshot)=>{
-        const data=snapshot.val();
-        setUrl(data.imageUrls);
-       
-       
-    });
+    const [url, setUrl] = useState();
+    useEffect(() => {
+        const usersRef = ref(database, '/users');
+        onValue(usersRef, (snapshot) => {
+            const data = snapshot.val();
+            setUrl(data.imageUrls);
 
-},[]);
 
-    return(
+        });
+
+    }, []);
+
+    return (
         <>
-           <Progress done="40"/>
-           <div className='heatmap' >
-           <h1 >No of Device = {No_of_devices}</h1>
-           <img src={url}/>
-           </div>
-           <br/>
-           <Link to="/upload/heatmap/select_support_plan" state={{ Plan: Plan, price:price, device:No_of_devices }}><button type="button" class="btn btn-outline-success"  onClick={()=>{dispatch(actionCreators.depositMoney(amount*(No_of_devices-1)))}}>continue</button></Link>
+            <Progress done="40" />
+            <div className="heatmap2_div_link">
+                <div className="card_heatmap2">
+                    <Link to="/upload/heatmap/select_support_plan" state={{ Plan: Plan, price: price, device: No_of_devices1 }} className="Heatmap2_link" onClick={() => { dispatch(actionCreators.depositMoney(amount * (No_of_devices1 - 1))) }}>
+                        <img src={url} alt="Heatmap" />
+                        <h1>Team A Algo</h1>
+                        <p className="price_heatmap2">Price : ${amount * (No_of_devices1)}</p>
+                        <p>Signal Strength : Strong</p>
+
+                    </Link>
+                </div>
+                <div className="card_heatmap2">
+                    <Link to="/upload/heatmap/select_support_plan" state={{ Plan: Plan, price: price, device: No_of_devices2 }} className="Heatmap2_link" onClick={() => { dispatch(actionCreators.depositMoney(amount * (No_of_devices2 - 1))) }}>
+
+                        <img src={url} alt="Heatmap" />
+                        <h1>Team B Algo</h1>
+                        <p className="price_heatmap2">Price : ${amount * (No_of_devices2)}</p>
+                        <p>Signal Strength : Poor</p>
+
+                    </Link>
+                </div>
+            </div>
         </>
     )
 }
